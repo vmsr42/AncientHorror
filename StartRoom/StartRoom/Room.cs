16f66cc6b238303ecea6e346576afea01acc2589
Ekaterina Voronich <lota@ngs.ru>
2015-09-07 20:11:45 +0600

    buttons

---
+++ StartRoom/StartRoom/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartRoom
+{
+    public class Room
+    {
+        public string RoomsName { get; set; }
+
+        public string LeadersName { get; set; }
+
+        public int PersonsNumbers { get; set; }
+    }
+}
