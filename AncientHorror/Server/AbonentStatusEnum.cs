3ad93797ca23c1fe7edf3ea6b1bae35c312b1762
vmsr42 <vmsr42@gmail.com>
2015-09-11 21:37:50 +0700

    Проверка

---
+++ AncientHorror/Server/AbonentStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientHorror.Server
+{
+    public enum AbonentStatusEnum
+    {
+        Guest,
+        Authorized,
+        Playing,
+        Disconnected
+    }
+}
